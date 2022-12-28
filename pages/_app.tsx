28c34f07c32b46219e2cd9fe24eca9d2d25d15ef
Fakepng <krit0kasemtewin@gmail.com>
2022-12-28 23:18:37 +0700

    add bootstrap

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,18 @@
 import "../styles/globals.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 import type { AppProps } from "next/app";
 import { SessionProvider } from "next-auth/react";
 import { ChakraProvider } from "@chakra-ui/react";
+import SSRProvider from "react-bootstrap/SSRProvider";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider>
-      <SessionProvider session={pageProps.session}>
-        <Component {...pageProps} />
-      </SessionProvider>
+      <SSRProvider>
+        <SessionProvider session={pageProps.session}>
+          <Component {...pageProps} />
+        </SessionProvider>
+      </SSRProvider>
     </ChakraProvider>
   );
 }
