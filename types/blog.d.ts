52df45798de0acb4cfffd4a445039ec6b287c01a
Fakepng <krit0kasemtewin@gmail.com>
2022-12-28 23:14:35 +0700

    type of blog

---
+++ types/blog.d.ts
@@ -0,0 +1,10 @@
+export type blogType = {
+  id: string;
+  title: string;
+  description?: string;
+  content: string;
+  authorId: string;
+  author: object;
+  createdAt: string;
+  updatedAt: string;
+};
